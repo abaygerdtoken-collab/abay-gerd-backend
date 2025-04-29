038afa6ab35db4d8cd3186620f32b76aaab98ed6
abaygerdtoken-collab <abaygerdtoken@gmail.com>
2025-04-29 17:55:32 -0400

    changed gas efficiency to 55000

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def send_token():
         tx = token_contract.functions.transfer(recipient, amount).build_transaction({
             'from': SENDER_ADDRESS,
             'nonce': nonce,
-            'gas': 70000,
+            'gas': 55000,
             'gasPrice': web3.to_wei('1', 'gwei')
         })
         signed_tx = web3.eth.account.sign_transaction(tx, private_key=PRIVATE_KEY)
