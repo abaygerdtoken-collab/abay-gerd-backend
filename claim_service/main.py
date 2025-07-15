d5ac648b61f48ad90ff89619d1815821e8409776
Abay GERD Token - Dev <abaygerdtoken@gmail.com>
2025-07-15 01:03:01 -0400

    Add files via upload

--- claim_service/main.py
+++ claim_service/main.py
@@ -553,7 +553,8 @@ def get_access_token():
 
     res = requests.post("https://account-d.docusign.com/oauth/token", headers=headers, data=data)
     if res.status_code != 200:
-        print("❌ DocuSign token error:", res.status_code, res.text)
+        import sys
+        print("❌ DocuSign token error:", res.status_code, res.text, file=sys.stdout, flush=True)
     res.raise_for_status()
     return res.json()["access_token"]
 
